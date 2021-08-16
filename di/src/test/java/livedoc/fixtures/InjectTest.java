518c1a9a02d7c381e2be3bfc150d94daa888def6
yizhou <yizhou.zhang@thoughtworks.com>
2021-08-16 13:59:54 +0800

    test: add inject test

---
+++ di/src/test/java/livedoc/fixtures/InjectTest.java
@@ -0,0 +1,11 @@
+package livedoc.fixtures;
+
+import com.thoughtworks.fusheng.integration.junit5.FuShengTest;
+
+@FuShengTest
+public class InjectTest extends BaseFixture {
+
+    public String tryGetDependencyNameInBar() {
+        return null;
+    }
+}

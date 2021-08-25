a11c26aae27fb27b61334d566fd17a13e272cc3c
yizhou <yizhou.zhang@thoughtworks.com>
2021-08-25 11:06:24 +0800

    test: refactor fusheng tests of ScopeTest

--- di/src/test/java/livedoc/fixtures/ScopeTest.java
+++ di/src/test/java/livedoc/fixtures/ScopeTest.java
@@ -1,31 +1,8 @@
 package livedoc.fixtures;
 
 import com.thoughtworks.fusheng.integration.junit5.FuShengTest;
-import livedoc.fixtures.objects.Buzz;
 
 @FuShengTest
 public class ScopeTest extends BaseFixture {
 
-    private Buzz buzz1;
-    private Buzz buzz2;
-
-    public void getFirstBuzz() throws ClassNotFoundException {
-        buzz1 = (Buzz) getInstance("Buzz");
-    }
-
-    public void getSecondBuzz() throws ClassNotFoundException {
-        buzz2 = (Buzz) getInstance("Buzz");
-    }
-
-    public void assertBuzzEquals() {
-        if (buzz1 == buzz2) {
-            throw new RuntimeException("Buzz equals");
-        }
-    }
-
-    public void assertFooEquals() {
-        if (buzz1.getFoo() != buzz2.getFoo()) {
-            throw new RuntimeException("Foo in buzz not equals");
-        }
-    }
 }

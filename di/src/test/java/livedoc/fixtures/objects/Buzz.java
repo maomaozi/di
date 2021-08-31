ae41ed1e69de487dc4596c51a7b07374aff0e4da
yizhou <yizhou.zhang@thoughtworks.com>
2021-08-31 14:35:09 +0800

    doc: optimize text in live doc

--- di/src/test/java/livedoc/fixtures/objects/Buzz.java
+++ di/src/test/java/livedoc/fixtures/objects/Buzz.java
@@ -4,15 +4,12 @@ package livedoc.fixtures.objects;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+@Singleton
 public class Buzz {
     private final Foo foo;
 
     @Inject
-    Buzz(@Singleton Foo foo) {
+    Buzz(Foo foo) {
         this.foo = foo;
-    }
-
-    public Foo getFoo() {
-        return foo;
     }
 }
